3583c56f58f67b0ef6a3c5155e0939e193d88397
Fatima Arellano <41347973+fatimaarellano@users.noreply.github.com>
2018-12-03 19:02:48 -0500

    Add files via upload

---
+++ php/variable.php
@@ -0,0 +1,11 @@
+<body>
+  <?php
+
+  $msg = "I like carrots"; //use the $ in front of var name to declare
+  $msg .= " and brocoli."; //use "." to apppend to an existing variable (make the variable flexiable)
+
+  echo $msg
+  //use echo to print message.
+
+  ?>
+</body>
